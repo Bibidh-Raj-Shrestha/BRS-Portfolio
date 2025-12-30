1776b1b45d2b770bbeb3f22674ef1aebe50073d2
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2025-12-30 10:03:36 +0545

    Added theme toggle to contact section

--- js/toggle.js
+++ js/toggle.js
@@ -1,16 +1,24 @@
+
+
 const toggle = document.getElementById("toggle");
 const toggle_ball = document.getElementById("toggle-ball");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
 const navbar = document.getElementById("navbar");
+const contactMe = document.getElementById("contact-me-content");
+
 
 let links = document.querySelectorAll("a");
 let headers = document.querySelectorAll("h2");
 let parag = document.querySelectorAll("p");
 let headers3 = document.querySelectorAll("h3");
 let headers4 = document.querySelectorAll("h4");
+let headers5 = document.querySelectorAll("h5");
+let labels = document.querySelectorAll("label");
+let input = document.querySelectorAll("input");
+let textArea = document.getElementById("message");
 
-let dark =[...links,...headers, ...parag, ...headers3,...headers4];
+
 
 let sections = document.querySelectorAll("section");
 let hero = document.getElementById("hero-section");
@@ -20,8 +28,16 @@ let skills_content =document.getElementsByClassName("skills-content");
 let skill_item = document.getElementsByClassName("skill-item");
 let skill_h3 = document.getElementsByClassName("skill-h3");
 let project = document.getElementsByClassName("project-item");
+let contact_links = document.getElementById("contact-links");
+let contact_info = document.getElementById("contact-info");
+let contact_form = document.getElementById("contact-form");
+let submit_btn = document.getElementById("submit-btn");
+
+let dark =[...links,...headers, ...parag, ...headers3,...headers4,...headers5,...labels,submit_btn];
+let inputs = [...input,textArea];
 
 let dark_divs =[...skill_item,...skill_h3,...project];
+let light_divs = [...skills_content,contact_info,contact_links,contact_form]
 
 toggle.addEventListener("click",()=>
 {
@@ -53,6 +69,17 @@ toggle.addEventListener("click",()=>
         dark_divs.forEach(el=>{
             el.style.backgroundColor="rgb(72, 72, 70)";
         })
+        foot.style.backgroundColor="rgb(72, 72, 70)";
+        light_divs.forEach(el=>{
+            el.style.backgroundColor="#3f3f3d";
+        })
+        contact_info.style.borderBottom = "#be5c46 2px solid";
+        contact_links.style.borderBottom = "#be5c46 2px solid";
+        inputs.forEach(el=>{
+            el.style.backgroundColor="#484846";
+        })
+        submit_btn.style.backgroundColor = "#ff5833c9";
+        contactMe.classList.remove("black-plc");
     }
     else{
         toggle_ball.classList.remove("toggle-ball-off");
@@ -73,17 +100,22 @@ toggle.addEventListener("click",()=>
             el.style.backgroundColor="rgba(235, 235, 235, 1)";
             el.style.border = "none";
         })
-        sections.forEach(el=>{
-            el.style.backgroundColor="rgba(235, 235, 235, 1)";
-        })
         hero.style.backgroundColor = "white";
-        skills_content.forEach(el=>{
-            el.style.backgroundColor="rgba(235, 235, 235, 1)";
-        })
+        
         dark_divs.forEach(el=>{
             el.style.backgroundColor="#d8d8d6ff";
         })
         foot.style.backgroundColor="rgba(235, 235, 235, 1)";
+        light_divs.forEach(el=>{
+            el.style.backgroundColor="rgba(235, 235, 235, 1)";
+            el.style.borderBottom = "none";
+        })
+        inputs.forEach(el=>{
+            el.style.backgroundColor="rgba(193, 193, 193, 1)";
+        })
+        submit_btn.style.backgroundColor = "#f0c5bdff";
+        contactMe.classList.add("black-plc");
+
     }
     
-});
+})
